cccafe57be0384554b15d673b77e332fd7c15ca9
Vipin Yadav <imvpn22@gmail.com>
2018-10-04 13:34:04 +0530

    refactor 2

--- restaurant-app/gulpfile.js
+++ restaurant-app/gulpfile.js
@@ -1,7 +1,6 @@
 /*eslint-env node */
 
 var gulp = require('gulp');
-var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync').create();
 var eslint = require('gulp-eslint');
@@ -13,39 +12,39 @@ var concat = require('gulp-concat');
 var minify = require('gulp-minify');
 
 gulp.task('css', function() {
-    return gulp.src('css/*.css')
-        .pipe(sourcemaps.init())
-        .pipe(concatCss('app.min.css'))
-        .pipe(cleanCSS())
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('build/css'));
+	return gulp.src('css/*.css')
+		.pipe(sourcemaps.init())
+		.pipe(concatCss('app.min.css'))
+		.pipe(cleanCSS())
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest('build/css'));
 });
 
 gulp.task('js', function() {
-    return gulp.src(['js/*.js'])
-        .pipe(sourcemaps.init())
-        // eslint() attaches the lint output to the eslint property
-        // of the file object so it can be used by other modules.
-        // .pipe(eslint())
-        // eslint.format() outputs the lint results to the console.
-        // Alternatively use eslint.formatEach() (see Docs).
-        // .pipe(eslint.format())
-        // To have the process exit with an error code (1) on
-        // lint error, return the stream and pipe to failOnError last.
-        // .pipe(eslint.failOnError())
-        .pipe(concat('app.js'))
-        // .pipe(minify())
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('build/js'))
+	return gulp.src(['js/*.js'])
+		.pipe(sourcemaps.init())
+		// eslint() attaches the lint output to the eslint property
+		// of the file object so it can be used by other modules.
+		// .pipe(eslint())
+		// eslint.format() outputs the lint results to the console.
+		// Alternatively use eslint.formatEach() (see Docs).
+		// .pipe(eslint.format())
+		// To have the process exit with an error code (1) on
+		// lint error, return the stream and pipe to failOnError last.
+		// .pipe(eslint.failOnError())
+		.pipe(concat('app.js'))
+		// .pipe(minify())
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest('build/js'));
 });
 
 gulp.task('default', gulp.series('css', 'js', function(done) {
-    gulp.watch('css/*.css', gulp.parallel('css'));
-    gulp.watch('js/ *.js', gulp.parallel('js'));
+	gulp.watch('css/*.css', gulp.parallel('css'));
+	gulp.watch('js/ *.js', gulp.parallel('js'));
 
-    // browserSync.init({
-    //     server: './'
-    // });
-    done();
+	// browserSync.init({
+	//     server: './'
+	// });
+	done();
 }));
 
--- restaurant-app/js/indexController.js
+++ restaurant-app/js/indexController.js
@@ -1,8 +1,8 @@
 if (navigator.serviceWorker) {
-    navigator.serviceWorker.register('/sw.js').then(function(reg) {
-        console.log('Worker registered!');
-        // console.log(reg);
-    }).catch(function(err) {
-        console.log(err);
-    });
+	navigator.serviceWorker.register('/sw.js').then(function(reg) {
+		console.log('Worker registered!');
+		console.log(reg);
+	}).catch(function(err) {
+		console.log(err);
+	});
 }
